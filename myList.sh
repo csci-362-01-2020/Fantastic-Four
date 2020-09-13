a9f33c7b690fb8cc2eb7ef7e88341c819fd9501b
Patrick McCabe <mccabepj@g.cofc.edu>
2020-09-13 18:35:18 -0400

    Updated Chrome Installer and repository updates

--- myList.sh
+++ myList.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# check for Google Chrome
-sudo apt install tree google-chrome-stable
+# check for updates and the Google Chrome package via aptitude package manager
+
+sudo apt update && sudo apt install google-chrome-stable
 
 # prints the current working directory
 # sed is an inline text editor that can accept text to be edited
