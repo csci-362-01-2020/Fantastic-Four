b192a64a02898a2ad68b86c493ffbd19d941f947
Patrick McCabe <mccabepj@g.cofc.edu>
2020-10-13 09:33:32 -0400

    Updated scripts

---
+++ TestAutomation/scripts/runAllTests.py
@@ -0,0 +1,19 @@
+#!/bin/env python3 
+
+import math
+import os
+import sys
+
+def main():
+
+    testFiles = []
+
+    for i in range(1, 26):
+        testFiles.append('testCase' + str(i) + '.txt')
+    
+    print(testFiles)
+    
+    
+
+
+main()
---
+++ TestAutomation/testCasesExecutables/testAdd.py
@@ -0,0 +1,9 @@
+#!/bin/env python3
+
+import math
+
+def main():
+    
+    print('test')
+
+main()
